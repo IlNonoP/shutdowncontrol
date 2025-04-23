a52f90701bfc612daa46a3f9e84c012a29220fc7
Samuele Oberti <IlNonoP@outlook.it>
2025-04-23 17:05:41 +0200

    Update watch.py

--- watch.py
+++ watch.py
@@ -2,10 +2,15 @@ import os
 path="/opt/shutdowncontrol/"
 
 while True:
-    if os.listdir(path+"lock_file/") == []:
-        print("No lock file found")
-        break
-    else:
-        print("Locked file found")
+    try:
+        file = os.listdir(path+"lock_file/")
+        if os.listdir(path+"lock_file/") == []:
+            print("No lock file found")
+            break
+        else:
+            print("Locked file found")
+    except:
+        pass
+    
     os.system("sleep 1")
 print("shoutdowncontroller terminate\n")
