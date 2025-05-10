18ba51989d9d2135e279b310fe98fbf19a11ceb0
Samuele Oberti <IlNonoP@outlook.it>
2025-05-10 14:43:31 +0200

    Update shutdowncontrol.py

--- shutdowncontrol.py
+++ shutdowncontrol.py
@@ -2,6 +2,8 @@ import sys
 import subprocess
 import os
 
+version="1.1"
+
 path="/opt/shutdowncontrol/"
 
 def main():
@@ -12,23 +14,62 @@ def main():
 
         
     if args[0] == "start":
-        try:            
-            port = args[1]
-            print("I'm using "+port +" port")
-            os.system("python3 server.py "+port)
+        try:
+            arg1=args[1]            
+            try:               
+                arg2=args[2]                
+            except:                
+                arg2 = "-"
+           
+
+            if "p=" in arg1:                
+                commodo = arg2
+                password = arg1
+                port = commodo
+            else:
+                password = arg2
+                port = arg1            
+            if port != "-":                
+                comando = "python3 server.py "+port+" "
+            else:                
+                comando = "python3 server.py - "            
+            if "p=" in password:
+                
+                password = password.replace("p=", "")
+                
+                if password == "-":
+                    print("\nERROR\nYou cant use this password, change it please")
+                    exit()
+                comando = comando + password + " "
+            else:
+                
+                comando = comando + "- "
+           
+            os.system(comando + version)
 
         except:
-            print("No port specified, 5000 will used")
-            os.system("python3 server.py")
+            print("No port specified or password specified, 5000 will used")
+            os.system("python3 server.py - - "+version)
+
+
     elif args[0] == "watch":
         os.system("python3 watch.py")
     elif args[0] == "help":
+        print("ShutdownContol V. "+version)
+        print("")
         print("Usage:")
         print("shoutdowncontrol [arg] ")
-        print("                    |-  start [port]                   This command start the server at the specified port")
-        print("                    |            |_ port  ex: 5000     (OPTIONAL!) (Default=5000) This number is the number of the port you will send the requests")
+        print("                    |-  start [args]                   This command start the server at the specified port")
+        print("                    |            |- [port]  ex: 5000     (OPTIONAL!) (Default=5000) This number is the number of the port you will send the requests")
+        print("                    |            |_ p=[password] ex: 123 (Optional!) (Default=Disable) This topic allows you to add a password to avoid unapproved requests")
         print("                    |-  watch                          This command is used for verify if there are any file lock. The program will continue only if tyhere isn't any lock, if tehere are any lock the program will not continue")
-        print("                    |_  help                           Show this guide")
+        print("                    |_  help                           Show this guide")  
+        print("")
+        print("Command example:")
+        print("shoutdowncontrol 5050                  Start shutdowncontrol server on 5050 port")
+        print("shuutdowncontrol 5050 p=pippo           Start the server on 5050 port and use pippo as password")
+        print("")
+        print("See https://github.com/IlNonoP/shutdowncontrol for a complete guide, and for report bug")
     else:
         print('Error, invalid command, use "shoutdowncontrol help" to see the guide')
 
